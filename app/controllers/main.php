d51061d42ed9b0cdcc8a53c3c79e002fc2b76211
Alvaro Cabrera <alvaro@outofthebox.mx>
2012-09-03 14:03:03 -0500

    Update main-controller definition

--- app/controllers/main.php
+++ app/controllers/main.php
@@ -4,7 +4,12 @@ class main_controller extends app_controller
 {
   function index()
   {
-    $this->add_style('main.css');
-    $this->body = partial('main.php');
+    $this->add_style('boilerplate.css');
+
+    $this->add_script('application.js');
+    $this->add_script('plugins.js');
+    $this->add_script('script.js');
+
+    echo partial('main.php');
   }
 }
